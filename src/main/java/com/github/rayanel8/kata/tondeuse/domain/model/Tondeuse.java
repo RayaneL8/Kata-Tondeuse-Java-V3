5b226c2655b185530e5cd786b87d802f238b30ee
Rayane <arayane.slimani@gmail.com>
2025-12-02 19:37:30 +0100

    Add first implementation of Tondeuse

--- src/main/java/com/github/rayanel8/kata/tondeuse/domain/model/Tondeuse.java
+++ src/main/java/com/github/rayanel8/kata/tondeuse/domain/model/Tondeuse.java
@@ -3,22 +3,64 @@ package com.github.rayanel8.kata.tondeuse.domain.model;
 import com.github.rayanel8.kata.tondeuse.domain.enums.Orientation;
 
 public class Tondeuse {
-    private Position position;
-    private Orientation orientation;
+	private Position position;
+	private Orientation orientation;
 
-    public Tondeuse(final Position newPosition, final Orientation newOrientation) {
-        position = newPosition;
-        orientation = newOrientation;
-    }
+	public Tondeuse(final Position newPosition, final Orientation newOrientation) {
+		position = newPosition;
+		orientation = newOrientation;
+	}
 
-    public void rotateLeft() {}
-    public void rotateRight() {}
-    public void moveForward(final Pelouse pelouse) {}
+	public Position getPosition() {
+		return position;
+	}
 
-    public Position getPosition() { return position; }
-    public void setPosition(final Position newPosition) { position = newPosition; }
-    
-    public Orientation getOrientation() { return orientation; }
-    public void setOrientation(final Orientation newOrientation) { orientation = newOrientation; }
-    
+	public Orientation getOrientation() {
+		return orientation;
+	}
+
+	public void rotateLeft() {
+		switch (orientation) {
+		case N -> orientation = Orientation.W;
+		case E -> orientation = Orientation.N;
+		case S -> orientation = Orientation.E;
+		case W -> orientation = Orientation.S;
+		}
+	}
+
+	public void rotateRight() {
+		switch (orientation) {
+		case N -> orientation = Orientation.E;
+		case E -> orientation = Orientation.S;
+		case S -> orientation = Orientation.W;
+		case W -> orientation = Orientation.N;
+		}
+	}
+
+	public void moveForward(final Pelouse pelouse) {
+		Position next = calculateNextPosition();
+
+		if (isInsidePelouse(next, pelouse)) {
+			position = next;
+		}
+	}
+
+	private Position calculateNextPosition() {
+		int x = position.getX();
+		int y = position.getY();
+
+		switch (orientation) {
+		case N -> y++;
+		case E -> x++;
+		case S -> y--;
+		case W -> x--;
+		}
+
+		return new Position(x, y);
+	}
+
+	private boolean isInsidePelouse(final Position p, final Pelouse pelouse) {
+		return p.getX() >= 0 && p.getX() <= pelouse.getMaxX() && p.getY() >= 0 && p.getY() <= pelouse.getMaxY();
+	}
+
 }
